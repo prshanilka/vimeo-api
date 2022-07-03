8761f071ecfe994d536f70c8fe084c3e26e26eb3
prshanilka <ramesh.shanilka@gmail.com>
2022-07-03 19:37:30 +0530

    readme

--- src/example/index.ts
+++ src/example/index.ts
@@ -1,21 +1,19 @@
 import { readFileSync } from 'fs';
-import { Readable } from 'stream';
 import Vimeo from '../index';
 
-// console.log(file);
 let data = readFileSync('./lib/example/mnv.mp4');
 const t = Vimeo({
-  token: '6131dbbec5e508b70236a80c52c7088f',
+  token: '',
 });
 
-// (async () => {
-//   try {
-//     const c = await t.uploadFileBuffer(data, 'ddd');
-//     console.log('ddd', c);
-//   } catch (error) {
-//     console.log(error);
-//   }
-// })();
+(async () => {
+  try {
+    const c = await t.uploadFileBuffer(data, 'ddd');
+    console.log('ddd', c);
+  } catch (error) {
+    console.log(error);
+  }
+})();
 
 (async () => {
   try {
